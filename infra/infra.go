69fdbb66edaeec9922788ee536d1e2131e993a58
tomocy <tomocy.dev@gmail.com>
2019-08-09 18:23:40 +0900

    add functions for config

--- infra/infra.go
+++ infra/infra.go
@@ -3,18 +3,59 @@ package infra
 import (
 	"encoding/json"
 	"io"
+	"os"
+	"path/filepath"
 	"time"
 
 	"golang.org/x/oauth2"
 )
 
+func today() time.Time {
+	now := time.Now()
+	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+}
+
+func loadConfig() (*config, error) {
+	name := configFilename()
+	src, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer src.Close()
+
+	var loaded *config
+	if err := readJSON(src, &loaded); err != nil {
+		return nil, err
+	}
+
+	return loaded, nil
+}
+
+func saveConfig(cnf *config) error {
+	name := configFilename()
+	dest, err := os.OpenFile(name, os.O_WRONLY, 0700)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	return writeJSON(dest, cnf)
+}
+
+func configFilename() string {
+	return filepath.Join(workspaceName(), "config.json")
+}
+
+func workspaceName() string {
+	return filepath.Join(os.Getenv("HOME"), "./tapioca")
+}
+
 func readJSON(src io.Reader, dest interface{}) error {
 	return json.NewDecoder(src).Decode(dest)
 }
 
-func today() time.Time {
-	now := time.Now()
-	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+func writeJSON(dest io.Writer, src interface{}) error {
+	return json.NewEncoder(dest).Encode(src)
 }
 
 type config struct {
