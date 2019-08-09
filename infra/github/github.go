59aad204c1c38e2678fa23a70aa60c2dc4eba651
tomocy <tomocy.dev@gmail.com>
2019-08-09 19:08:39 +0900

    add Diff

--- infra/github/github.go
+++ infra/github/github.go
@@ -1,6 +1,8 @@
 package github
 
-import "github.com/tomocy/tapioca/domain"
+import (
+	"github.com/tomocy/tapioca/domain"
+)
 
 type Commits []*Commit
 
@@ -14,11 +16,35 @@ func (cs Commits) Adapt() []*domain.Commit {
 }
 
 type Commit struct {
-	SHA string `json:"sha"`
+	SHA   string  `json:"sha"`
+	Files []*Diff `json:"files"`
 }
 
 func (c *Commit) Adapt() *domain.Commit {
 	return &domain.Commit{
-		ID: c.SHA,
+		ID:   c.SHA,
+		Diff: c.adaptDiff(),
+	}
+}
+
+func (c *Commit) adaptDiff() *domain.Diff {
+	if len(c.Files) <= 0 {
+		return nil
+	}
+
+	return c.Files[0].Adapt()
+}
+
+type Diff struct {
+	Changes   int `json:"changes"`
+	Additions int `json:"additions"`
+	Deletions int `json:"deletions"`
+}
+
+func (d *Diff) Adapt() *domain.Diff {
+	return &domain.Diff{
+		Changes: d.Changes,
+		Adds:    d.Additions,
+		Dels:    d.Deletions,
 	}
 }
