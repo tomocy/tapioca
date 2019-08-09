9e57c5fd67af5789aa632c56119c6a375be34612
tomocy <tomocy.dev@gmail.com>
2019-08-09 18:19:07 +0900

    add config and githubConfig to represent a config of all and github

--- infra/infra.go
+++ infra/infra.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io"
 	"time"
+
+	"golang.org/x/oauth2"
 )
 
 func readJSON(src io.Reader, dest interface{}) error {
@@ -13,4 +15,12 @@ func readJSON(src io.Reader, dest interface{}) error {
 func today() time.Time {
 	now := time.Now()
 	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+}
+
+type config struct {
+	github githubConfig
+}
+
+type githubConfig struct {
+	AccessToken *oauth2.Token
 }
