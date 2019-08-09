e07511182c4071045e8da3703c6d895abe9a1279
tomocy <tomocy.dev@gmail.com>
2019-08-10 00:28:31 +0900

    add report to report error with failed action

--- cmd/tapioca/client/client.go
+++ cmd/tapioca/client/client.go
@@ -1,9 +1,21 @@
 package client
 
+import "fmt"
+
 type config struct {
 	repo repo
 }
 
 type repo struct {
 	owner, name string
+}
+
+func reportFunc(did string) func(err error) error {
+	return func(err error) error {
+		return report(did, err)
+	}
+}
+
+func report(did string, err error) error {
+	return fmt.Errorf("failed to %s: %s", did, err)
 }
