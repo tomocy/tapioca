5200e22cd04ab230534f61af439001d02fa1720c
tomocy <tomocy.dev@gmail.com>
2019-08-09 18:31:33 +0900

    add createWorkspace to create workspace

--- infra/infra.go
+++ infra/infra.go
@@ -10,6 +10,28 @@ import (
 	"golang.org/x/oauth2"
 )
 
+func createWorkspace() error {
+	name := configFilename()
+	if _, err := os.Stat(name); err == nil {
+		return nil
+	}
+
+	dir := workspaceName()
+	if err := os.RemoveAll(dir); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		return err
+	}
+
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+
+	return f.Close()
+}
+
 func today() time.Time {
 	now := time.Now()
 	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
