19c06c2fb5714ce6abef1e194da1dbca2bb47157
tomocy <tomocy.dev@gmail.com>
2019-08-09 23:57:51 +0900

    add (*CommitUsecase).FetchCommits to fetch commits of given owner and repo

--- app/usecase.go
+++ app/usecase.go
@@ -5,3 +5,7 @@ import "github.com/tomocy/tapioca/domain"
 type CommitUsecase struct {
 	repo domain.CommitRepo
 }
+
+func (u *CommitUsecase) FetchCommits(owner, repo string) (domain.Commits, error) {
+	return u.repo.FetchCommits(owner, repo)
+}
