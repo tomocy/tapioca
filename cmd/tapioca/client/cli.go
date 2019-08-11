aae4cb5cd78567ecac1b189c01a0f5088ccfd59f
tomocy <tomocy.dev@gmail.com>
2019-08-11 23:04:04 +0900

    add (*CLI).summarizeCommitsOfToday to summarize commits of today and show the result

--- cmd/tapioca/client/cli.go
+++ cmd/tapioca/client/cli.go
@@ -15,6 +15,25 @@ func (c *CLI) Run() error {
 	return c.fetchCommits()
 }
 
+func (c *CLI) summarizeCommitsOfToday() error {
+	report := reportFunc("summarize commits of today")
+	cnf, err := c.parseConfig()
+	if err != nil {
+		flag.Usage()
+		return report(err)
+	}
+
+	uc := newCommitUsecase()
+	s, err := uc.SummarizeCommitsOfToday(cnf.repo.owner, cnf.repo.name)
+	if err != nil {
+		return report(err)
+	}
+
+	c.showSummary(s)
+
+	return nil
+}
+
 func (c *CLI) fetchCommits() error {
 	report := reportFunc("fetch commits")
 	cnf, err := c.parseConfig()
@@ -50,6 +69,10 @@ func (c *CLI) parseConfig() (*config, error) {
 			name:  splited[1],
 		},
 	}, nil
+}
+
+func (*CLI) showSummary(s *domain.Summary) {
+	fmt.Println(s)
 }
 
 func (*CLI) showCommits(cs domain.Commits) {
