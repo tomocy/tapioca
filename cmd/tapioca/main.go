598bf081badde9bef0b66c98c682fe2fb2320f4c
tomocy <tomocy.dev@gmail.com>
2019-08-12 00:03:43 +0900

    modify main to use selected client

--- cmd/tapioca/main.go
+++ cmd/tapioca/main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	runner := new(client.CLI)
-	if err := runner.Run(); err != nil {
+	c := client.New()
+	if err := c.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "failed to run: %s\n", err)
 		os.Exit(1)
 	}
