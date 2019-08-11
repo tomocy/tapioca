232e17148d8d7ccf0b43bf8cf2cf61ee64bfd4a3
tomocy <tomocy.dev@gmail.com>
2019-08-11 23:24:44 +0900

    add ColorizedSummary for Summary to be colorized and legible string

--- cmd/tapioca/client/view/ascii/ascii.go
+++ cmd/tapioca/client/view/ascii/ascii.go
@@ -8,6 +8,19 @@ import (
 	"github.com/tomocy/tapioca/domain"
 )
 
+type ColorizedSummary domain.Summary
+
+func (s ColorizedSummary) String() string {
+	white := color.New(color.FgWhite).FprintfFunc()
+	var b strings.Builder
+	white(
+		&b, "summary of commits to %s in %s\n%s",
+		s.Repo, s.Date.Format("2006/01/02"), ColorizedDiff(*s.Diff),
+	)
+
+	return b.String()
+}
+
 type ColorizedDiff domain.Diff
 
 func (d ColorizedDiff) String() string {
