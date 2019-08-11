d55f2104176fbb16f542d7d6fab10fdad0d0b6e0
tomocy <tomocy.dev@gmail.com>
2019-08-12 00:30:58 +0900

    add (*Text).PrintSummary to print summary with text

--- cmd/tapioca/client/format/format.go
+++ cmd/tapioca/client/format/format.go
@@ -1,3 +1,14 @@
 package format
 
+import (
+	"fmt"
+	"io"
+
+	"github.com/tomocy/tapioca/domain"
+)
+
 type Text struct{}
+
+func (t *Text) PrintSummary(w io.Writer, s domain.Summary) {
+	fmt.Fprintln(w, s)
+}
