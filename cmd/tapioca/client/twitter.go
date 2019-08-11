c630b3c8f902526b60ac12cc26af9f22ce11c0e3
tomocy <tomocy.dev@gmail.com>
2019-08-12 01:09:01 +0900

    add (*Twitter).Run to summarizeCommitsOfToday

--- cmd/tapioca/client/twitter.go
+++ cmd/tapioca/client/twitter.go
@@ -1,5 +1,12 @@
 package client
 
+import (
+	"net/url"
+
+	"github.com/pkg/browser"
+	"github.com/tomocy/tapioca/domain"
+)
+
 func newTwitter(cnf config) *Twitter {
 	return &Twitter{
 		cnf: cnf,
@@ -8,4 +15,31 @@ func newTwitter(cnf config) *Twitter {
 
 type Twitter struct {
 	cnf config
+}
+
+func (t *Twitter) Run() error {
+	return t.summarizeCommitsOfToday()
+}
+
+func (t *Twitter) summarizeCommitsOfToday() error {
+	report := reportFunc("summarize commits of today")
+
+	uc := newCommitUsecase()
+	s, err := uc.SummarizeCommitsOfToday(t.cnf.repo.owner, t.cnf.repo.name)
+	if err != nil {
+		return report(err)
+	}
+
+	t.showSummary(*s)
+
+	return nil
+}
+
+func (t *Twitter) showSummary(s domain.Summary) {
+	parsed, _ := url.Parse("https://twitter.com/intent/tweet")
+	parsed.RawQuery = url.Values{
+		"text": []string{s.String()},
+	}.Encode()
+
+	browser.OpenURL(parsed.String())
 }
