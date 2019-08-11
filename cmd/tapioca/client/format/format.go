d56bbc7516b86816f4a9ce486a30e62dfaebf0f3
tomocy <tomocy.dev@gmail.com>
2019-08-12 00:41:38 +0900

    add (*Color).PrintSummary to print summary with color

--- cmd/tapioca/client/format/format.go
+++ cmd/tapioca/client/format/format.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/tomocy/tapioca/cmd/tapioca/client/format/ascii"
 	"github.com/tomocy/tapioca/domain"
 )
 
@@ -14,3 +15,7 @@ func (t *Text) PrintSummary(w io.Writer, s domain.Summary) {
 }
 
 type Color struct{}
+
+func (c *Color) PrintSummary(w io.Writer, s domain.Summary) {
+	fmt.Fprintln(w, ascii.ColorizedSummary(s))
+}
