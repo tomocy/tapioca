5fa12e9ed8d902203eb1d3323f008857af59522b
tomocy <tomocy.dev@gmail.com>
2019-08-12 03:41:46 +0900

    move codes

--- cmd/tapioca/client/mode.go
+++ cmd/tapioca/client/mode.go
@@ -2,14 +2,27 @@ package client
 
 import (
 	"net/url"
+	"os"
 
 	"github.com/pkg/browser"
 	"github.com/tomocy/tapioca/domain"
 )
 
-type Twitter struct {
-	cnf config
+func newCLI(printer printer) *CLI {
+	return &CLI{
+		printer: printer,
+	}
 }
+
+type CLI struct {
+	printer printer
+}
+
+func (c *CLI) ShowSummary(s domain.Summary) {
+	c.printer.PrintSummary(os.Stdout, s)
+}
+
+type Twitter struct{}
 
 func (t *Twitter) ShowSummary(s domain.Summary) {
 	parsed, _ := url.Parse("https://twitter.com/intent/tweet")
