4bdebcdd04cbc6c450dab1f6ffdd827daddea6b3
tomocy <tomocy.dev@gmail.com>
2019-12-31 18:13:33 +0900

    add text
    This text will replace the old one in client package.

--- cmd/tapioca/main.go
+++ cmd/tapioca/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/fatih/color"
 	"github.com/tomocy/tapioca/app"
 	"github.com/tomocy/tapioca/domain"
 	"github.com/tomocy/tapioca/infra"
@@ -104,4 +107,45 @@ type config struct {
 
 type presenter interface {
 	PresentSummary(domain.Summary)
+}
+
+type text struct {
+	colorized bool
+}
+
+func (p *text) PrintSummary(w io.Writer, s domain.Summary) {
+	if p.colorized {
+		fmt.Fprintln(w, colorizedSummary(s))
+		return
+	}
+
+	fmt.Fprintln(w, s)
+}
+
+type colorizedSummary domain.Summary
+
+func (s colorizedSummary) String() string {
+	white := color.New(color.FgWhite).FprintfFunc()
+
+	var b strings.Builder
+	white(
+		&b, "summary of commits to %s in %s\n%s",
+		s.Repo, s.Since.Format("2006/01/02"), colorizedDiff(*s.Diff),
+	)
+
+	return b.String()
+}
+
+type colorizedDiff domain.Diff
+
+func (d colorizedDiff) String() string {
+	green, red := color.New(color.FgGreen).FprintfFunc(), color.New(color.FgRed).FprintfFunc()
+
+	var b strings.Builder
+	fmt.Fprintf(&b, "%d changes: ", d.Changes)
+	green(&b, "%d adds", d.Adds)
+	fmt.Fprint(&b, ", ")
+	red(&b, "%d dels", d.Dels)
+
+	return b.String()
 }
