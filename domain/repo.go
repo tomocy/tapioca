1bbbb64fdae0f1ceb04cefc451a15e605ddb8b4f
tomocy <tomocy.dev@gmail.com>
2019-12-31 19:24:13 +0900

    add RepoRepo
    RepoRepo represents a repository for the repository on github of the given owner.

--- domain/repo.go
+++ domain/repo.go
@@ -1,5 +1,9 @@
 package domain
 
+type RepoRepo interface {
+	FetchRepos(owner string) ([]*Repo, error)
+}
+
 type CommitRepo interface {
 	FetchCommits(owner, repo string, params Params) (Commits, error)
 }
