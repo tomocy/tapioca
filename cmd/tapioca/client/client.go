9c5b2a35bc0fba5dd3edbb6ee4d9471d169aab3e
tomocy <tomocy.dev@gmail.com>
2019-12-31 19:48:11 +0900

    add (*OfRepos).Run
    (*OfRepos).Run fetches and summarizes the commits of the repositories of the authorized user.

--- cmd/tapioca/client/client.go
+++ cmd/tapioca/client/client.go
@@ -23,6 +23,28 @@ type OfRepos struct {
 	presenter     Presenter
 }
 
+func (c *OfRepos) Run() error {
+	ss, err := c.summarize()
+	if err != nil {
+		return err
+	}
+
+	for _, s := range ss {
+		c.presenter.PresentSummary(*s)
+	}
+
+	return nil
+}
+
+func (c *OfRepos) summarize() ([]*domain.Summary, error) {
+	u := newRepoUsecase()
+	return u.SummarizeCommits(c.owner, domain.Params{
+		Author: c.author,
+		Since:  c.since,
+		Until:  c.until,
+	})
+}
+
 func NewOfRepo(owner, repo, author string, since, until time.Time, presenter Presenter) *OfRepo {
 	return &OfRepo{
 		owner: owner, repo: repo, author: author,
@@ -57,9 +79,15 @@ func (c *OfRepo) summarize() (*domain.Summary, error) {
 	})
 }
 
-func newCommitUsecase() *app.CommitUsecase {
-	return app.NewCommitUsecase(infra.NewGitHub())
+func newRepoUsecase() *app.RepoUsecase {
+	return app.NewRepoUsecase(githubRepo, newCommitUsecase())
 }
+
+func newCommitUsecase() *app.CommitUsecase {
+	return app.NewCommitUsecase(githubRepo)
+}
+
+var githubRepo = infra.NewGitHub()
 
 type Presenter interface {
 	PresentSummary(domain.Summary)
